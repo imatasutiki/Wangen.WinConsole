8d2d758b31e79552ca076607bf4122ae11642718
imatasutiki <{ID}+{username}@users.noreply.github.com>
2021-03-02 12:56:45 +0100

    Encapsula el IFileConcrete pensando en el proceso Merge

---
+++ Toys/Parts/Merge/Archive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nibiru.Urubamba.Contracts.Atoms.All;
+
+namespace Nibiru.Wangen.WinConsole.Toys.Parts.Merge
+{
+    class Archive
+    {
+        IFileConcrete _file;
+        public Archive(
+            IFileConcrete file)
+        {
+            this._file = file;
+        }
+        public IFileConcrete File { get { return this._file; } }
+    }
+}
