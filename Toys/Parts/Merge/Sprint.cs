c76ed6220d135dd88b7aa5895d00d51a22de5184
imatasutiki <{ID}+{username}@users.noreply.github.com>
2021-03-02 13:03:37 +0100

    Encapsula IDirectoryConcrete

---
+++ Toys/Parts/Merge/Sprint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nibiru.Urubamba.Contracts.Atoms.All;
+
+namespace Nibiru.Wangen.WinConsole.Toys.Parts.Merge
+{
+    class Sprint
+    {
+        IDirectoryConcrete _directory;
+
+        public Sprint(
+            IDirectoryConcrete directory )
+        {
+            this._directory = directory;
+        }
+        public IDirectoryConcrete Directory { get { return this._directory; } }
+    }
+}
